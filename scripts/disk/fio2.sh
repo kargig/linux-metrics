f106fec66e486efa5ba952685a6a0b1793ce418a
tzermias <zacharias.tzermias@gmail.com>
2018-11-10 21:56:48 +0200

    Fix minor typo in fio2.sh
    This commit fixes the misplaced --norandommap fio flag.

--- scripts/disk/fio2.sh
+++ scripts/disk/fio2.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-fio --directory=/tmp --name fio_test_file --direct=1 --rw=randread --bs=16k --size=100M --numjobs=16 --time_based --runtime=180 --group_reporting
---norandommap
+fio --directory=/tmp --name fio_test_file --direct=1 --rw=randread --bs=16k --size=100M --numjobs=16 --time_based --runtime=180 --group_reporting --norandommap
